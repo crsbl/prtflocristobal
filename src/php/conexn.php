25da4357edf64463ae6482670d4688f73aef165e
crsbl <83726556+crsbl@users.noreply.github.com>
2021-11-06 12:24:25 -0300

    asd
    asd

--- src/php/conexn.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-
-    $conn = mysqli_connect("db4free.net", "prflo3", "dow1230Sodw","bd_prf33");
-    if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
-    }
-?>
