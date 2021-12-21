9bbabc817dad4f88aaeead76b6940ff78f38c766
crsbl <83726556+crsbl@users.noreply.github.com>
2021-12-21 12:12:52 -0300

    estado tono
    cambio vuelvo a true

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const [std_exm, setstd_exm] = useState(0)
 
 const [std_mnu, setstd_mnu] = useState(0)
 
-const [std_tono, setstd_tono] = useState(false)
+const [std_tono, setstd_tono] = useState(true)
 
 
 let std_croll = null
